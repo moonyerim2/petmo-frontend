4bcc06fe601a47997220f10771b08443c919ea1e
moonyerim2 <moonyerim2@gmail.com>
2022-11-08 19:26:31 +0900

    feat: 사용자 ip 주소 얻어오는 useIp 훅 추가

---
+++ src/api/ip.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const callGetIpApi = async () => {
+  try {
+    const response = await axios.get("https://api.ipify.org/");
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
