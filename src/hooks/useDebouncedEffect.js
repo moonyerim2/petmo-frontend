ca8826ac9dcdbb2e91587946279261fd523d9add
moonyerim2 <moonyerim2@gmail.com>
2022-11-08 22:14:50 +0900

    feat: useDebouncedEffect 훅 추가

---
+++ src/hooks/useDebouncedEffect.js
@@ -0,0 +1,17 @@
+import { useCallback, useEffect } from "react";
+
+const useDebouncedEffect = (func, delay, deps) => {
+  const callback = useCallback(func, deps);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      callback();
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [callback, delay]);
+};
+
+export default useDebouncedEffect;
