b019ae211e41e220910ce2869a97a82c6d263c85
booyaChael <chaeeul2022@gmail.com>
2022-11-10 17:17:29 +0900

    feat: nav 중간까지(아이콘 색깔 설정 필요)

--- src/components/home/feed/Nav.js
+++ src/components/home/feed/Nav.js
@@ -1,27 +1,59 @@
 import React from "react";
 import styled from "styled-components";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import FeedIcon from "../../../css/icon/FeedIcon.svg";
+import BookmarkIcon from "../../../css/icon/BookmarkIcon.svg";
+import ProfileIcon from "../../../css/icon/ProfileIcon.svg";
 
-const Wrapper = styled.div`
-  width: 100%;
+const Wrapper = styled(Box)`
   height: 57px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   position: fixed;
   bottom: 0;
   background-color: white;
 `;
+const NavButtonBox = styled(BottomNavigation)`
+  &.MuiBottomNavigation-root {
+    width: 320px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 20px;
+  }
+`;
 
+const NavButton = styled(BottomNavigationAction)`
+  &.MuiButtonBase-root {
+    padding: 0px;
+    margin: 0px;
+  }
+  &&.Mui-selected {
+    color: #fa3c89;
+  }
+`;
+
+const Icon = styled.img``;
 function Nav() {
+  const [value, setValue] = React.useState(0);
+
   return (
-    <Wrapper>
-      <span>피드</span>
-      &nbsp;
-      <span>관심목록</span>
-      &nbsp;
-      <span>프로필</span>
+    <Wrapper sx={{ width: 500 }}>
+      <NavButtonBox
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <NavButton label="피드" icon={<Icon src={FeedIcon} />} />
+        <NavButton label="북마크" icon={<Icon src={BookmarkIcon} />} />
+        <NavButton label="프로필" icon={<Icon src={ProfileIcon} />} />
+      </NavButtonBox>
     </Wrapper>
   );
 }
-
 export default Nav;
