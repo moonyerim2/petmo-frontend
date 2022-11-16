a0e618128590f48d947b05a484370a7c41c3b714
moonyerim2 <moonyerim2@gmail.com>
2022-11-17 00:48:49 +0900

    refactor: SearchBarAnimal 컴포넌트 디자인과 다른 부분 수정 - emotion -> styled-component로 변경

--- src/components/home/feed/searchBar/SearchBarAnimal.js
+++ src/components/home/feed/searchBar/SearchBarAnimal.js
@@ -2,42 +2,38 @@ import * as React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
-import styled from "@emotion/styled";
+import styled from "styled-components";
+import { tags } from "../../../../constants";
 
 const Wrapper = styled(Box)`
-  background-color: #f1f1f5;
+  background-color: ${({ theme: { colors } }) => colors.bgMedium};
   width: 100%;
   height: 56px;
-  display: flex;
-  align-items: center;
-  padding: 5px 0px;
-`;
-const TagBox = styled(Tabs)`
-  &&.MuiTabs-indicator {
-  }
+  padding: 10px 0 10px 20px;
+  font-family: Pretendard, -apple-system, BlinkMacSystemFont, "Malgun Gothic",
+    "맑은 고딕", helvetica, "Apple SD Gothic Neo", sans-serif;
 `;
 
 const Tag = styled(Tab)`
   &&.MuiButtonBase-root {
-    min-width: 0px;
-    background-color: #ffffff;
-    width: 69px;
-    height: 36px;
-    font-family: "Pretendard";
-    font-style: normal;
+    background-color: ${({ theme: { colors } }) => colors.white};
     font-weight: 500;
     font-size: 14px;
     line-height: 100%;
-    border: 1px solid #f0f0f6;
     border-radius: 200px;
-    color: #000000;
-    margin: 4px;
-    padding: 11px 0px;
+    color: ${({ theme: { colors } }) => colors.black};
+    margin-right: 8px;
   }
+
   &&.Mui-selected {
-    background: #111111;
-    border-radius: 200px;
-    color: #ffffff;
+    background: ${({ theme: { colors } }) => colors.black};
+    color: ${({ theme: { colors } }) => colors.white};
+  }
+
+  &&.MuiTab-root {
+    padding: 11px 16px;
+    min-width: 0;
+    min-height: 36px;
   }
 `;
 
@@ -49,8 +45,8 @@ export default function SearchBarAnimal() {
   };
 
   return (
-    <Wrapper sx={{ height: 46, padding: 0 }}>
-      <TagBox
+    <Wrapper>
+      <Tabs
         value={value}
         onChange={handleClick}
         variant="scrollable"
@@ -63,15 +59,10 @@ export default function SearchBarAnimal() {
         }}
       >
         <Tag label="전체" />
-        <Tag label="강아지" />
-        <Tag label="고양이" />
-        <Tag label="물고기" />
-        <Tag label="햄스터" />
-        <Tag label="파충류" />
-        <Tag label="새" />
-        <Tag label="토끼" />
-        <Tag label="기타" />
-      </TagBox>
+        {tags.pet.map((species) => (
+          <Tag key={species} label={species} />
+        ))}
+      </Tabs>
     </Wrapper>
   );
 }
