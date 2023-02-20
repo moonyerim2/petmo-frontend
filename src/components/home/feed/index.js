bede6152bc1b9cc8135979aa20581aa68b240bf0
moonyerim2 <moonyerim2@gmail.com>
2023-02-20 21:23:20 +0900

    feat: 사용하지 않는 컴포넌트 삭제

--- src/components/home/feed/index.js
+++ src/components/home/feed/index.js
@@ -1,6 +1,6 @@
 export { default as SearchBar } from "./searchBar/SearchBar";
 export { default as Board } from "./Board";
-export { default as CardList } from "./CardList";
 export { default as FeedHeader } from "./FeedHeader";
 export { default as Nav } from "../../common/Nav";
+export { default as PostList } from "./PostList";
 export { default as WritePostBtn } from "./WritePostBtn";
