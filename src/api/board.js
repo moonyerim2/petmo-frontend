b991dca79d9a4226444a14330e43652fda47608e
moonyerim2 <moonyerim2@gmail.com>
2023-02-24 13:29:24 +0900

    feat: 게시글 단건 조회 api 추가 [feat/21]

--- src/api/board.js
+++ src/api/board.js
@@ -1,6 +1,18 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
 
+export const callSinglePostApi = async (postId) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/board/${postId}`);
+
+    if (response.status !== 200) throw new Error("Request faild");
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
+
 export const callPostsApi = async ({
   boardAddress,
   animalTypes,
