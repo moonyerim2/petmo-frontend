b7e8e3752f1c1320830726b283a46f67bda50dd2
moonyerim2 <moonyerim2@gmail.com>
2023-02-24 13:27:43 +0900

    refactor: Post 내부 구조 SummaryCard 컴포넌트로 분리 [feat/21]

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,5 +1,6 @@
 export * from "./cardFooter";
 export * from "./contents";
+export { default as SummaryCard } from "./SummaryCard";
 export { default as FullCard } from "./FullCard";
 export { default as Card } from "./Card";
 export { default as Content } from "./Content";
