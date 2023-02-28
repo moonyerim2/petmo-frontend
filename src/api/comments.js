878250b671e89ad9089825cc8eb7b23d3c51ed76
moonyerim2 <moonyerim2@gmail.com>
2023-02-28 18:11:20 +0900

    feat: 댓글 등록 api 추가 [feat/26]

--- src/api/comments.js
+++ src/api/comments.js
@@ -12,3 +12,27 @@ export const callCommentsApi = async (postId) => {
     return error.response;
   }
 };
+
+export const callAddCommentsApi = async (payload) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/comments/add`, payload);
+
+    if (response.status !== 201) throw new Error("Request faild");
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
+
+export const callAddReplyCommentsApi = async (payload) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/comments/reply`, payload);
+
+    if (response.status !== 201) throw new Error("Request faild");
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
