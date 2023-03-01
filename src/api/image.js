a5e214f784b4814b1473469e17171922148b8346
moonyerim2 <moonyerim2@gmail.com>
2023-03-02 00:30:43 +0900

    feat: 이미지 업로드 api 추가 [feat/32]

--- src/api/image.js
+++ src/api/image.js
@@ -1,6 +1,25 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
 
+export const callUploadImageApi = async (payload) => {
+  try {
+    const response = await axios({
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      url: `${BASE_URL}/image/upload`,
+      method: "POST",
+      data: payload,
+    });
+
+    if (response.status !== 201) throw new Error("Request faild");
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error.response;
+  }
+};
+
 export const callSingleImageUrlApi = async (imageId) => {
   try {
     const response = await axios.get(`${BASE_URL}/image/${imageId}`);
