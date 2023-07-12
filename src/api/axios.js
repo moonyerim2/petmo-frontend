7fadf9a97f39dff60dd078e72ad59f549a2e11f9
moonyerim2 <moonyerim2@gmail.com>
2023-07-13 00:23:48 +0900

    feat: csrf 토큰 요청 헤더에 추가

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
+import { getCookie } from "../util";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
+    "X-CSRFToken": getCookie("csrftoken") || "",
   },
 });
