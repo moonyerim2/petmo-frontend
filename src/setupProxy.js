42c803a79b8cbf04d344483f500ee8aa7d089ad2
sliverKi <lovee2756@gmail.com>
2023-08-26 14:42:47 +0900

    Correct typo in proxy target site address.

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/api/v2", {
-      target: "https://petmo-back-seed-db-lqoa.onrender.come",
+      target: "https://petmo-back-seed-db-lqoa.onrender.com",
       changeOrigin: true,
     })
   );
