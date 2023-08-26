903b74417fe522bbe067f1e32370a8b5698c82bb
sliverKi <lovee2756@gmail.com>
2023-08-26 14:37:18 +0900

    Feat: setting Proxy.

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware("/api/v2", {
+      target: "https://petmo-back-seed-db-lqoa.onrender.come",
+      changeOrigin: true,
+    })
+  );
+};
