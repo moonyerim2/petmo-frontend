2abf00151e11a1019ddd0e094eb49960da04aa8b
sliverKi <lovee2756@gmail.com>
2023-08-27 16:43:08 +0900

    Modified callPostsApi

--- src/api/board.js
+++ src/api/board.js
@@ -1,6 +1,6 @@
 import { axiosInstance as axios } from "./axios";
 import { BASE_URL } from "../constants";
-
+import { getCookie } from "../util";
 export const callSinglePostApi = async (postId) => {
   try {
     const response = await axios.get(`${BASE_URL}/posts/${postId}`);
@@ -13,19 +13,34 @@ export const callSinglePostApi = async (postId) => {
   }
 };
 
-export const callPostsApi = async ({
-  boardAddress,
-  animalTypes,
-  categoryType,
-  lastBoardId,
-}) => {
+// export const callPostsApi = async ({
+//   boardAddress,
+//   animalTypes,
+//   categoryType,
+//   lastBoardId,
+// }) => {
+//   try {
+//     const encodedUrl = encodeURI(
+//       `${BASE_URL}/posts/list?boardAddress=${boardAddress}&animalTypes=${animalTypes}&categoryType=${categoryType}&lastBoardId=${lastBoardId}`
+//     );
+
+//     const response = await axios.get(encodedUrl);
+
+//     if (response.status !== 200) throw new Error("Request faild");
+//     return response;
+//   } catch (error) {
+//     console.log(error);
+//     return error.response;
+//   }
+// };
+
+export const callPostsApi = async (payload) => {//게시글 리스트 불러오기
   try {
-    const encodedUrl = encodeURI(
-      `${BASE_URL}/posts/list?boardAddress=${boardAddress}&animalTypes=${animalTypes}&categoryType=${categoryType}&lastBoardId=${lastBoardId}`
-    );
-
-    const response = await axios.get(encodedUrl);
-
+    const response = await axios.post(`${BASE_URL}/posts/`, payload, {
+      headers: {
+        "X-CSRFToken": getCookie("csrftoken"),
+      },
+    });
     if (response.status !== 200) throw new Error("Request faild");
     return response;
   } catch (error) {
